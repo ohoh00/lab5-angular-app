27fda9e14469c4b25924d00de3d03a5643adeb37
ohoh00 <ohoh2425@hotmail.com>
2020-05-07 00:47:14 +0700

    frist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab5-angular-app';
+  parentColorProperty : string='';
+  receiveData($event){
+    this.parentColorProperty = $event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgclassComponent } from './components/ngclass/ngclass.component';
+import { NgstyleComponent } from './components/ngstyle/ngstyle.component';
+import { NgifComponent } from './components/ngif/ngif.component';
+import { NgswitchComponent } from './components/ngswitch/ngswitch.component';
+import { NgforComponent } from './components/ngfor/ngfor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgclassComponent,
+    NgstyleComponent,
+    NgifComponent,
+    NgswitchComponent,
+    NgforComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
